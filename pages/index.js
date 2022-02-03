ef71490d8aa465a49b9257be39a00a59a24ec609
ACH RIZAL <achrizal15@gmail.com>
2022-02-03 10:34:33 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,5 @@
-
-const url = "http://localhost:3000/api/pemasukan/";
-export default function Home(data) {
-  let pemasukan = data.data
-  const handleClick = async (id) => {
-    let pems = await fetch(url + id, { method: "DELETE" });
-    let result = await pems.json()
-    console.log(result.response);
-  }
-  const handleSubmit = async (event) => {
-    event.preventDefault()
-    const data = event.target.nama.value;
-    try {
-      const tambah = await fetch(url, {
-        method: "POST", body: JSON.stringify({ nama: data }), headers: {
-          'Content-Type': 'application/json'
-        },
-      })
-      console.log(tambah);
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  return (
-    <div>
-      {pemasukan.map((v, k) => {
-        return (<div key={k + 1}><button onClick={()=> handleClick(v._id)} className='bg-sky-500 px-3 py-2 rounded-full m-2 text-white'>{v.nama}</button></div>)
-      })}
-      <form method='POST' id='formadd' onSubmit={handleSubmit}>
-        <input name='nama' type="text" />
-        <button type='submit' >SUBMIT</button>
-      </form>
-    </div>
-  )
-}
-export async function getServerSideProps(context) {
-  const res = await fetch(url, {
-    method: "GET"
-  });
-  const data = await res.json()
-  return {
-    props: { data }, // will be passed to the page component as props
-  }
-}
+export default (first) => {
+ return(
+   <div>Halo</div>
+ )
+};
